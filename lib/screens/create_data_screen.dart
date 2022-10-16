d0518697f311b831b8f79aece85cb32780b61557
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-10-16 09:06:24 +0330

    hive_undo

---
+++ lib/screens/create_data_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+import '../model_class.dart';
+
+class CreateDataScreen extends StatefulWidget {
+  const CreateDataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateDataScreen> createState() => _CreateDataScreenState();
+}
+
+class _CreateDataScreenState extends State<CreateDataScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    _phoneController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Data'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: _ageController,
+                decoration: const InputDecoration(labelText: 'Age'),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: _phoneController,
+                decoration: const InputDecoration(labelText: 'Phone'),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  final value = ModelClass(
+                    name: _nameController.text,
+                    age: int.parse(_ageController.text),
+                    phone: int.parse(_phoneController.text),
+                    status: true,
+                  );
+
+                  Hive.box('PersonBox').add(value);
+                },
+                child: const Text('Create'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
